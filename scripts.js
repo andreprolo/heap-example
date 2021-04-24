20a4f9de5c5cd755564f97016c81bcc075ac594f
André Prolo <proloandre@hotmail.com>
2021-04-24 10:01:51 -0300

    Refactoring and optimizing script

--- scripts.js
+++ scripts.js
@@ -1,74 +1,129 @@
-var heap = []
+function Heap() {
+    this.items = []
 
-function loadHeapView() {
-    let heapTag = document.getElementById("heap-view");
-    heapTag.innerHTML = "";
+    this.push = () => {
+        let error = document.getElementById("error");
+        error.innerText = "";
 
-    let size = heap.length;
-    let topItem = size == 0 ? '' : heap[size-1];
+        let input = document.getElementById("value");
 
-    if (size == 0) {
-        let content = document.createElement('div');
-        content.className = 'empty';
-        heapTag.appendChild(content);
-    } else {
-        heap.reverse().forEach((value, index) => {
+        let value = input.value;
+
+        input.value = "";
+
+        if (!value) {
+            error.innerText = "Valor não informado";
+            return;
+        }
+
+        if (value > 1000 || value < -999) {
+            error.innerText = "O valor deve ser entre: -999 e 999";
+            return;
+        }
+        this.items.push(value);
+
+        this.updateHeapView();
+    }
+
+    this.pop = () => {
+        if (this.items.length == 0) {
+            return;
+        }
+    
+        this.items.pop();
+        this.updateHeapView();
+    }
+
+    this.clear = () => {
+        this.items = [];
+        this.updateHeapView();
+    }
+
+    this.updateHeapView = () => {
+        let heapTag = document.getElementById("heap-view");
+
+        let size = this.items.length;
+        let viewSize = this.getViewSize(heapTag);
+
+        if (size <= 0) {
+            heapTag.innerHTML = "";
             let content = document.createElement('div');
-            content.className = 'value';
-
-            if (index == size-1 && size > 1) {
-                content.className += ' base';
-            } else if (index == 0 && size > 2) {
-                content.className += ' last';
-            }
-            content.innerText = value;
+            content.className = 'empty';
             heapTag.appendChild(content);
+
+            this.updateSizeTag(0);
+            this.updateTopItemTag("");
+            return;
+        } else {
+            this.removeEmptyItem(heapTag);
+        }
+
+        if (size == viewSize - 1) {
+            heapTag.removeChild(heapTag.firstElementChild);
+        } else if (size == viewSize + 1) {
+            let content = document.createElement('div');
+            
+            content.innerText = this.items[this.items.length-1];
+
+            heapTag.prepend(content);
+        }
+
+        this.createClassNameForItems(heapTag);
+        this.updateSizeTag(size);
+        this.updateTopItemTag(this.items[size-1])
+    }
+
+    this.removeEmptyItem = (heapTag) => {
+        if (this.items.length > 1) return;
+        heapTag.childNodes.forEach(item => {
+            if (item.className.includes('empty')) item.parentNode.removeChild(item);            
         });
     }
 
-    let sizeTag = document.getElementById("size");
-    sizeTag.innerText = `Tamanho: ${size}`;
-
-    let topItemTag = document.getElementById("top-item");
-    topItemTag.innerText = `Objeto topo: ${topItem}`
-}
-
-function pushValue() {
-    let error = document.getElementById("error");
-    error.innerText = "";
-
-    let input = document.getElementById("value");
-
-    let value = input.value;
-
-    input.value = "";
-
-    if (!value) {
-        error.innerText = "Valor não informado";
-        return;
+    this.getViewSize = (heapTag) => {
+        let size = 0;
+        heapTag.childNodes.forEach(item => {
+            if (!item.className.includes('empty')) size++;            
+        });
+        return size;
     }
 
-    if (value > 1000 || value < -999) {
-        error.innerText = "O valor deve ser entre: -999 e 999";
-        return;
+    this.updateSizeTag = (size) => {
+        let sizeTag = document.getElementById("size");
+        sizeTag.innerText = `Tamanho: ${size}`;
     }
 
-    heap.push(value);
-    loadHeapView();
-
-}
-
-function popValue() {
-    if (heap.length == 0) {
-        return;
+    this.updateTopItemTag = (topItem) => {
+        let topItemTag = document.getElementById("top-item");
+        topItemTag.innerText = `Objeto topo: ${topItem}`;
     }
 
-    heap.pop();
-    loadHeapView();
+    this.createClassNameForItems = (heapTag) => {
+        if (heapTag.childNodes.length < 1) return;
+        heapTag.childNodes.forEach((item, index) => {     
+            if (!item.className.includes('value')) {
+                item.className = 'value';
+            }
+
+            if (item.className.includes('last') && index > 0 && index != heapTag.childNodes.length - 1) {
+                item.className = item.className.replaceAll('last', '');
+            }
+
+            if (index == 0) {                 
+                if (heapTag.childNodes.length < 3) {
+                    item.className = item.className.replaceAll('last', '');
+                } else if (!item.className.includes('last')) {
+                    item.className += ' last';
+                }
+            } else if (index == heapTag.childNodes.length - 1) {
+                if (heapTag.childNodes.length < 2) {
+                    item.className = item.className.replaceAll('base', '');
+                } else if (!item.className.includes('base')) {
+                    item.className += ' base';
+                }
+            }
+        });
+    }
 }
 
-function clearHeap() {
-    heap = [];
-    loadHeapView();
-}
-
+const heap = new Heap();
