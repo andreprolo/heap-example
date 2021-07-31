0b2d98669023ae2bdd5c9b6de3bfa521465f03bf
Andre Prolo <proloandre@hotmail.com>
2021-07-31 15:02:37 -0300

    Renaming some variables

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function Heap() {
+function Stack() {
     this.items = []
 
     this.push = () => {
@@ -22,65 +22,65 @@ function Heap() {
         }
         this.items.push(value);
 
-        this.updateHeapView();
+        this.updateStackView();
     }
 
     this.pop = () => {
         if (this.items.length == 0) return;
     
         this.items.pop();
-        this.updateHeapView();
+        this.updateStackView();
     }
 
     this.clear = () => {
         this.items = [];
-        this.updateHeapView();
+        this.updateStackView();
     }
 
-    this.updateHeapView = () => {
-        let heapTag = document.getElementById("heap-view");
+    this.updateStackView = () => {
+        let stackTag = document.getElementById("stack-view");
 
         let size = this.items.length;
-        let viewSize = this.getViewSize(heapTag);
+        let viewSize = this.getViewSize(stackTag);
 
         if (size <= 0) {
-            heapTag.innerHTML = "";
+            stackTag.innerHTML = "";
             let content = document.createElement('div');
             content.className = 'empty';
-            heapTag.appendChild(content);
+            stackTag.appendChild(content);
 
             this.updateSizeTag(0);
             this.updateTopItemTag("");
             return;
         } else {
-            this.removeEmptyItem(heapTag);
+            this.removeEmptyItem(stackTag);
         }
 
         if (size == viewSize - 1) {
-            heapTag.removeChild(heapTag.firstElementChild);
+            stackTag.removeChild(stackTag.firstElementChild);
         } else if (size == viewSize + 1) {
             let content = document.createElement('div');
             
             content.innerText = this.items[this.items.length-1];
 
-            heapTag.prepend(content);
+            stackTag.prepend(content);
         }
 
-        this.createClassNameForItems(heapTag);
+        this.createClassNameForItems(stackTag);
         this.updateSizeTag(size);
         this.updateTopItemTag(this.items[size-1])
     }
 
-    this.removeEmptyItem = (heapTag) => {
+    this.removeEmptyItem = (stackTag) => {
         if (this.items.length > 1) return;
-        heapTag.childNodes.forEach(item => {
+        stackTag.childNodes.forEach(item => {
             if (item.className.includes('empty')) item.parentNode.removeChild(item);            
         });
     }
 
-    this.getViewSize = (heapTag) => {
+    this.getViewSize = (stackTag) => {
         let size = 0;
-        heapTag.childNodes.forEach(item => {
+        stackTag.childNodes.forEach(item => {
             if (!item.className.includes('empty')) size++;            
         });
         return size;
@@ -96,25 +96,25 @@ function Heap() {
         topItemTag.innerText = `Objeto topo: ${topItem}`;
     }
 
-    this.createClassNameForItems = (heapTag) => {
-        if (heapTag.childNodes.length < 1) return;
-        heapTag.childNodes.forEach((item, index) => {     
+    this.createClassNameForItems = (stackTag) => {
+        if (stackTag.childNodes.length < 1) return;
+        stackTag.childNodes.forEach((item, index) => {     
             if (!item.className.includes('value')) {
                 item.className = 'value';
             }
 
-            if (item.className.includes('last') && index > 0 && index != heapTag.childNodes.length - 1) {
+            if (item.className.includes('last') && index > 0 && index != stackTag.childNodes.length - 1) {
                 item.className = item.className.replaceAll('last', '');
             }
 
             if (index == 0) {                 
-                if (heapTag.childNodes.length < 3) {
+                if (stackTag.childNodes.length < 3) {
                     item.className = item.className.replaceAll('last', '');
                 } else if (!item.className.includes('last')) {
                     item.className += ' last';
                 }
-            } else if (index == heapTag.childNodes.length - 1) {
-                if (heapTag.childNodes.length < 2) {
+            } else if (index == stackTag.childNodes.length - 1) {
+                if (stackTag.childNodes.length < 2) {
                     item.className = item.className.replaceAll('base', '');
                 } else if (!item.className.includes('base')) {
                     item.className += ' base';
@@ -124,4 +124,4 @@ function Heap() {
     }
 }
 
-const heap = new Heap();
+const stack = new Stack();
